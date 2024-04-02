292ffc47f92b1d83bb16d17f6e710f740ade174e
Robert Wood <L.robertwood@gmail.com>
2024-04-01 22:28:40 -0400

    edited getusers endpoint

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -114,7 +114,7 @@ const onSubmit = async (event) => {
       // sets variable for the useApiStore for endpoint calling
       const apiStore1 = useApiStore();
       // call endpoint and send request through api store
-      await apiStore1.setApiData('https://api.revmetrix.io/api/getusers'); /* NEED TO MAKE THIS A VARIABLE */
+      await apiStore1.setApiData('https://api.revmetrix.io/api/gets/getusers'); /* NEED TO MAKE THIS A VARIABLE */
 
       // navigate to the result page
       await router.push({ name: 'result' });
